98f6b79e708fe35481a183b95df714fb515e649a
Kirill Filonchuk <kir.realtor@gmail.com>
2023-02-11 11:50:42 +0200

    lesson 3 step-1

---
+++ backend/models/UserModel.js
@@ -0,0 +1,18 @@
+const { model, Schema } = require("mongoose");
+
+const usersSchema = Schema({
+  userName: {
+    type: String,
+    default: "Jhon Konor",
+  },
+  userEmail: {
+    type: String,
+    required: [true, "DB: Field Email is required"],
+  },
+  userPassword: {
+    type: String,
+    required: [true, "DB: Field Password is required"],
+  },
+});
+
+module.exports = model("users", usersSchema);
